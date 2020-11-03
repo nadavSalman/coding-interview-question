7fb0ea80c4ed91fc4d0198f96d0b292220002174
נדב סלמן <nadavops@gmail.com>
2020-11-03 22:26:45 +0200

    add falied test for CI testing

--- src/test/java/coding/interview/question/AppTest.java
+++ src/test/java/coding/interview/question/AppTest.java
@@ -11,4 +11,9 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void failTest() {
+        App classUnderTest = new App();
+        assertTrue(false);
+    }
 }
